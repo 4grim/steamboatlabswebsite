86f358069f34967de9bb3e4482a76d326e3817a7
4grim <amber.grimaldi@gmail.com>
2014-02-14 13:05:08 -0800

    adding MediaCling to upload files in heroku

--- steamboatweb/settings.py
+++ steamboatweb/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
--- steamboatweb/wsgi.py
+++ steamboatweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "steamboatweb.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from dj_static import Cling, MediaCling
 
-application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application())
