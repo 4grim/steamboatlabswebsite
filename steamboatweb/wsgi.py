fb25102736a1a30cf63f9fbcaa228f5f8657143c
4grim <amber.grimaldi@gmail.com>
2014-02-17 14:12:43 -0800

    Edit media files to be served with S3 and not use cling media

--- steamboatweb/wsgi.py
+++ steamboatweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "steamboatweb.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from dj_static import Cling
 
-application = Cling(MediaCling(get_wsgi_application())
+application = Cling(get_wsgi_application())
