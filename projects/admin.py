84671c7b8d241eddd327b55eaea7c3c230eb9160
4grim <amber.grimaldi@gmail.com>
2014-02-17 17:36:12 -0800

    updating projects admin to include ledia links

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from projects.models import Project, ProjectImage, Client
+from projects.models import Project, ProjectImage, Client, MediaLinks
 
 
 admin.site.register(Project)
 admin.site.register(ProjectImage)
 admin.site.register(Client)
+admin.site.register(MediaLinks)
 
