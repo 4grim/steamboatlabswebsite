e0e98ff22d75105f845c6f630a4606b318500ba9
4grim <amber.grimaldi@gmail.com>
2014-02-17 23:55:54 -0800

    working on getting project media links to work

--- projects/models.py
+++ projects/models.py
@@ -9,15 +9,6 @@ class ProjectImage(models.Model):
 		return self.title
 
 
-class MediaLink(models.Model):
-	publication = models.CharField(max_length=200)
-	link = models.URLField()
-	quote = models.TextField(blank=True)
-
-	def __unicode__(self):
-		return self.publication
-
-
 class Client(models.Model):
 	company_name = models.CharField(max_length=200, default='')
 	company_logo = models.ImageField(upload_to='projects', blank=True, default='')
@@ -38,10 +29,17 @@ class Project(models.Model):
 	feature_project = models.BooleanField()
 	images = models.ManyToManyField(ProjectImage, blank=True)
 	client = models.ForeignKey(Client)
-	media_links = models.ManyToManyField(MediaLink, blank=True)
 
 	def __unicode__(self):
 		return self.title
 
 
+class MediaLink(models.Model):
+	publication = models.CharField(max_length=200)
+	link = models.URLField()
+	quote = models.TextField(blank=True)
+	project = models.ForeignKey(Project)
+
+	def __unicode__(self):
+		return self.publication
 
