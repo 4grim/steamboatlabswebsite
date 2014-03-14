073c2099c1e669179ed748e3f42e19934a028a91
4grim <amber.grimaldi@gmail.com>
2014-03-13 18:25:33 -0700

    updating blog url to accept hyphens in slug

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
 	url(r'^archive/$', views.archive, name='archive'),
 	url(r'^categories/(?P<category_id>\d+)/$', views.index_of_category, name='index_of_category'),
 	url(r'^authors/(?P<author_id>\d+)/$', views.index_of_author, name='index_of_author'),
-	url(r'^tags/(?P<slug>\w+)/$', views.index_of_tag, name='index_of_tag'),
+	url(r'^tags/(?P<slug>[a-zA-Z0-9-_]+)/$', views.index_of_tag, name='index_of_tag'),
 	url(r'^(?P<slug>[a-zA-Z0-9-_]+)/$', views.entry, name='entry'),
 	)
